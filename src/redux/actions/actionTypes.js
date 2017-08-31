805dd24ef6f61c901495587871afb9ebb78951b0
Eric Newman <eric@numonium.com>
2017-08-31 13:32:55 -0400

    add redux + scaffolding

---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,18 @@
+// 1. general API stuff
+export const API_OK = 'API_OK';
+export const API_NO = 'API_NO';
+
+// 2. global loading flag
+export const LOADING_TRUE = 'LOADING_TRUE';
+export const LOADING_FALSE = 'LOADING_FALSE';
+
+// 3. load token
+export const TOKEN_USE = 'TOKEN_USE';
+
+export const YT_SEARCH_QUERIED = 'YT_SEARCH_QUERIED';
+export const YT_SEARCH_RECEIVED_OK = 'YT_SEARCH_RECEIVED_OK';
+export const YT_SEARCH_RECEIVED_NO = 'YT_SEARCH_RECEIVED_NO';
+
+export const YT_SEARCH_LAZY_LOAD_QUERIED = 'YT_SEARCH_LAZY_LOAD_QUERIED';
+export const YT_SEARCH_LAZY_LOAD_RECEIVED_OK = 'YT_SEARCH_LAZY_LOAD_RECEIVED_OK';
+export const YT_SEARCH_LAZY_LOAD_RECEIVED_NO = 'YT_SEARCH_LAZY_LOAD_RECEIVED_NO';
